159fbda18d54dad00aee9121405069de21efd1f8
Tarek Morshed <tarek.morshid@gmail.com>
2019-02-20 12:37:05 +0200

    fan-bug

--- fan_control.py
+++ fan_control.py
@@ -7,7 +7,7 @@ from gpiozero import PWMLED
 class FanControl(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.fan = PWMLED(12)
+        self.fan = PWMLED(18)
 
     def run(self):
         while True:
